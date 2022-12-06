91fb8a5f686d9794575853174ee7583b05d97696
Ariel Chenet <apchenet@gmail.com>
2022-12-06 20:19:24 +0100

    Add actual functionality

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,37 @@ async fn index() -> Result<NamedFile, std::io::Error> {
 }
 
 #[post("/", data = "<n>")]
-fn generate(n: Form<i32>) -> Template {
+fn generate(n: Form<usize>) -> Template {
+    let n = n.into_inner();
+    let mut out: Vec<Vec<usize>> = vec![];
+    let a = (1..=n).collect();
+    gen(n, a, &mut out);
     Template::render(
         "response",
         context! {
-            n: n.into_inner(),
-            output: vec![1],
+            n: n,
+            output: out,
         },
     )
+}
+
+fn gen(k: usize, a: Vec<usize>, out: &mut Vec<Vec<usize>>) {
+    if k == 1 {
+        out.push(a.clone());
+        return;
+    }
+    gen(k - 1, a.clone(), out);
+    for i in 0..(k - 1) {
+        let b = swap(i, k - 1, a.clone());
+        gen(k - 1, b, out);
+    }
+}
+
+fn swap(i: usize, j: usize, mut b: Vec<usize>) -> Vec<usize> {
+    let t = b[i];
+    b[i] = b[j];
+    b[j] = t;
+    b
 }
 
 #[launch]
